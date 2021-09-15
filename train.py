a8da179faeaa7d34ae74544e90d51879034b064b
Dima <gluh.nia@gmail.com>
2021-09-15 22:40:25 +0300

    dataloader creating

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 import argparse
 from utils.model import yolov4
 from utils.utils import show_torch_image
+from utils.utils import dataload
 # === device ===
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -14,6 +15,11 @@ img = cv2.resize(cv2.imread('./test.jpg'), (628, 628))
 image = torch.from_numpy(img) / 255.0
 image = image.permute(2, 0, 1).unsqueeze(0).to(device)
 # print('image.shape before: {0}'.format(image.shape))
+
+# === dataset loader ===
+path = os.path.join('./data/')
+imgsz = 224
+dataset = dataload(path_to_images=path, img_size=224)
 
 # === model ===
 model = yolov4()
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import cv2
 import numpy as np
@@ -8,4 +9,24 @@ def show_torch_image(torch_image):
         image = image.detach().numpy()
         cv2.imshow('111', image)
         cv2.waitKey()
+    return None
+
+def dataload(path_to_images, img_size=640):
+    extensions = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG']
+    tree = os.walk(path_to_images)
+    list_of_images = []
+    for folder1, folder2, names in tree:
+        for file in names:
+            name, extension = [x for x in file.split('.')]
+
+            if extension in extensions:
+                if os.path.exists(folder1 + name + '.txt'):
+                    print(name + '.' + extension, ':')
+                    # get image from folder
+                    image = cv2.imread(folder1 + name + '.' + extension)
+                    if not image is None:
+                        image = cv2.resize(image, (img_size, img_size))
+                        print(image.shape)
+                    else:
+                        None
     return None
